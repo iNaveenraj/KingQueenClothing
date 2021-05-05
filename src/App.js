fb597f65b0af45f4e83de7254cb6c026aee2b4e5
naveen <krinaveenraj@gmail.com>
2021-05-05 13:52:24 +0530

    Redux changes for set user name

--- src/App.js
+++ src/App.js
@@ -5,32 +5,30 @@ import {Route, Switch} from "react-router-dom";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import {auth, CreateUserProfileDocument} from './firebase/firebase.utils';   
+import {connect} from 'react-redux';
+import {setCurrentUser} from './redux/user/user-action';
 class App extends React.Component{
-  constructor(){
-    super();
-    this.state={
-      Currentuser : null
-    };
-  }
   
   unSubscirbeUser = null;
   
   componentDidMount(){
+    
+    const {setCurrentUser} = this.props
      this.unSubscirbeUser = auth.onAuthStateChanged(async userAuth=>{         // -> this is method always listen to publisher and trigger when there is change in the sign in state 
         //console.log(user);              // --> user object holds complete information about the current user signed in
         if(userAuth){
+        
           var userRef = await CreateUserProfileDocument(userAuth);
           userRef.onSnapshot((snapshot) =>{
-            console.log(snapshot);
-            this.setState({
-              CurrentUser :{
-                id : snapshot.id,
-                ...snapshot.data() 
-              }
-            });
-          })
+           console.log(this.props.setCurrentuser);
+           setCurrentUser({
+            id : snapshot.id,
+            ...snapshot.data() 
+        });
+           
+          });
         }else{
-          this.setState({CurrentUser: null});
+          setCurrentUser({CurrentUser: null});
         }
       
        // this.setState({Currentuser:user})
@@ -41,12 +39,10 @@ class App extends React.Component{
     this.unSubscirbeUser();   // --> Not understood .... as per video used to close listener..
   }
 
-  
-
   render(){
     return (
       <div className="App">
-      <Header CurrentUser = {this.state.Currentuser}></Header>
+      <Header></Header>
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route exact path ="/shop" component={ShopPage}></Route> 
@@ -58,6 +54,9 @@ class App extends React.Component{
 
 }
 
+const mapDispatchToProps  = dispatch => ({
+  setCurrentUser : user => dispatch(setCurrentUser(user))
+});
   
 
-export default App;
+export default connect(null, mapDispatchToProps)(App);
